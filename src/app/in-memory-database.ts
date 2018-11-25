3da0c1114c9fa301cccb1c35d47e81ffda8d7d0e
Andrew Rosário <andrew.arosario@gmail.com>
2018-11-25 21:10:17 -0200

    adicionando categt model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,16 +1,17 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Category } from './pages/categories/shared/category.model';
 
 export class InMemoryDatabase implements InMemoryDbService {
 
   createDb() {
 
-    const categories = [
-      { id: 1, name: 'Lazer', description: 'Cinema, parques, shows' },
-      { id: 1, name: 'Moradia', description: 'Pagamento das contas da casa' },
-      { id: 1, name: 'Transporte', description: 'Combustível, Uber' },
-      { id: 1, name: 'Saúde', description: 'Plano de saúde e remédios' },
-      { id: 1, name: 'Salário', description: 'Recebimento de salário' },
-      { id: 1, name: 'Freelas', description: 'Trabalhos como freelancer' },
+    const categories: Category = [
+      { id: '1', name: 'Lazer', description: 'Cinema, parques, shows' },
+      { id: '2', name: 'Moradia', description: 'Pagamento das contas da casa' },
+      { id: '3', name: 'Transporte', description: 'Combustível, Uber' },
+      { id: '4', name: 'Saúde', description: 'Plano de saúde e remédios' },
+      { id: '5', name: 'Salário', description: 'Recebimento de salário' },
+      { id: '6', name: 'Freelas', description: 'Trabalhos como freelancer' },
     ];
 
     return { categories };

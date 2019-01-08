25294626d73d2fb5d5c768d40fafd20ed0557f25
Andrew Rosário <andrew.arosario@gmail.com>
2019-01-08 19:55:46 -0200

    finalizando o projeto

--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
 import { Category } from '../../categories/shared/category.model';
-import { Entry } from '../../entries/shared/entry.model';
-import { EntryService } from '../../entries/shared/entry.service';
 import { CategoryService } from '../../categories/shared/category.service';
 
-import * as currencyFormatter from 'currency-formatter';
+import { Entry } from '../../entries/shared/entry.model';
+import { EntryService } from '../../entries/shared/entry.service';
+
+import currencyFormatter from 'currency-formatter';
 
 @Component({
   selector: 'app-reports',
@@ -22,13 +24,11 @@ export class ReportsComponent implements OnInit {
 
   chartOptions = {
     scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true
-          }
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
         }
-      ]
+      }]
     }
   };
 
@@ -38,29 +38,25 @@ export class ReportsComponent implements OnInit {
   @ViewChild('month') month: ElementRef = null;
   @ViewChild('year') year: ElementRef = null;
 
-  constructor(
-    private entryService: EntryService,
-    private categoryService: CategoryService
-  ) { }
+  constructor(private entryService: EntryService, private categoryService: CategoryService) { }
 
   ngOnInit() {
     this.categoryService.getAll()
       .subscribe(categories => this.categories = categories);
   }
 
-  public generateReports() {
+  generateReports() {
     const month = this.month.nativeElement.value;
     const year = this.year.nativeElement.value;
 
-    if (!month || !year ) {
-      alert('Você precisa selecionar o mês e o ano para gerar os relatórios!');
-      return;
+    if (!month || !year) {
+      alert('Você precisa selecionar o mês e o ano para gerar os relatórios');
+    } else {
+      this.entryService.getByMonthAndYear(month, year)
+        .subscribe(this.setValues.bind(this));
     }
-
-    this.entryService.getByMonthAndYear(month, year)
-      .subscribe(this.setValues.bind(this));
-
   }
+
 
   private setValues(entries: Entry[]) {
     this.entries = entries;
@@ -68,27 +64,30 @@ export class ReportsComponent implements OnInit {
     this.setChartData();
   }
 
+
   private calculateBalance() {
     let expenseTotal = 0;
     let revenueTotal = 0;
 
     this.entries.forEach(entry => {
       if (entry.type === 'revenue') {
-        revenueTotal += currencyFormatter.unformat(entry.amount, { code: 'BLR' });
+        revenueTotal += currencyFormatter.unformat(entry.amount, { code: 'BRL' });
       } else {
-        expenseTotal += currencyFormatter.unformat(entry.amount, { code: 'BLR' });
+        expenseTotal += currencyFormatter.unformat(entry.amount, { code: 'BRL' });
       }
     });
 
-    this.revenueTotal = currencyFormatter.format(revenueTotal, { code: 'BRL' });
-    this.expenseTotal = currencyFormatter.format(expenseTotal, { code: 'BRL' });
-    this.balance = currencyFormatter(revenueTotal - expenseTotal, { code: 'BRL' });
+    this.expenseTotal = currencyFormatter.format(expenseTotal, { code: 'BRL'});
+    this.revenueTotal = currencyFormatter.format(revenueTotal, { code: 'BRL'});
+    this.balance = currencyFormatter.format(revenueTotal - expenseTotal, { code: 'BRL'});
   }
+
 
   private setChartData() {
     this.revenueChartData = this.getChartData('revenue', 'Gráfico de Receitas', '#9CCC65');
     this.expenseChartData = this.getChartData('expense', 'Gráfico de Despesas', '#e03131');
   }
+
 
   private getChartData(entryType: string, title: string, color: string) {
     const chartData = [];
@@ -102,7 +101,7 @@ export class ReportsComponent implements OnInit {
       // if found entries, then sum entries amount and add to chartData
       if (filteredEntries.length > 0) {
         const totalAmount = filteredEntries.reduce(
-          (total, entry) => total + currencyFormatter.unformat(entry.amount, { code: 'BLR' }), 0
+          (total, entry) => total + currencyFormatter.unformat(entry.amount, { code: 'BRL' }), 0
         );
 
         chartData.push({
